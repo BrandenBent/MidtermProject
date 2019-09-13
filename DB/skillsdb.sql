2441bc11a3f90120533bccd7c8cb92be72b6cffe
Mark Ag <mk2modx@icloud.com>
2019-09-13 13:28:43 -0600

    Approved DB

--- DB/skillsdb.sql
+++ DB/skillsdb.sql
@@ -30,6 +30,177 @@ CREATE TABLE IF NOT EXISTS `user` (
   UNIQUE INDEX `username_UNIQUE` (`username` ASC))
 ENGINE = InnoDB;
 
+
+-- -----------------------------------------------------
+-- Table `skill`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `skill` ;
+
+CREATE TABLE IF NOT EXISTS `skill` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `description` TEXT NULL,
+  `supplies` TEXT NULL,
+  `summary` TEXT NULL,
+  `prerequisite_skill_id` INT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_skill_skill_idx` (`prerequisite_skill_id` ASC),
+  CONSTRAINT `fk_skill_skill`
+    FOREIGN KEY (`prerequisite_skill_id`)
+    REFERENCES `skill` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `requirement`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `requirement` ;
+
+CREATE TABLE IF NOT EXISTS `requirement` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(1000) NOT NULL,
+  `description` TEXT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `profile`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `profile` ;
+
+CREATE TABLE IF NOT EXISTS `profile` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `birthday` DATE NOT NULL,
+  `login_time` VARCHAR(45) NULL,
+  `image_link` VARCHAR(1000) NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_profile_user_idx` (`user_id` ASC),
+  CONSTRAINT `fk_profile_user`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `resource`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `resource` ;
+
+CREATE TABLE IF NOT EXISTS `resource` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `image_link` VARCHAR(1000) NULL,
+  `video_link` VARCHAR(1000) NULL,
+  `site_link` VARCHAR(1000) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `achievement`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `achievement` ;
+
+CREATE TABLE IF NOT EXISTS `achievement` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `skill_id` INT NOT NULL,
+  `profile_id` INT NOT NULL,
+  `date_started` DATE NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_skill_has_profile_profile1_idx` (`profile_id` ASC),
+  INDEX `fk_skill_has_profile_skill1_idx` (`skill_id` ASC),
+  CONSTRAINT `fk_skill_has_profile_skill1`
+    FOREIGN KEY (`skill_id`)
+    REFERENCES `skill` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_skill_has_profile_profile1`
+    FOREIGN KEY (`profile_id`)
+    REFERENCES `profile` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `skill_requirement`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `skill_requirement` ;
+
+CREATE TABLE IF NOT EXISTS `skill_requirement` (
+  `requirement_id` INT NOT NULL,
+  `skill_id` INT NOT NULL,
+  `step_number` INT NOT NULL,
+  PRIMARY KEY (`requirement_id`, `skill_id`),
+  INDEX `fk_requirement_has_skill_skill1_idx` (`skill_id` ASC),
+  INDEX `fk_requirement_has_skill_requirement1_idx` (`requirement_id` ASC),
+  CONSTRAINT `fk_requirement_has_skill_requirement1`
+    FOREIGN KEY (`requirement_id`)
+    REFERENCES `requirement` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_requirement_has_skill_skill1`
+    FOREIGN KEY (`skill_id`)
+    REFERENCES `skill` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `skill_resource`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `skill_resource` ;
+
+CREATE TABLE IF NOT EXISTS `skill_resource` (
+  `skill_id` INT NOT NULL,
+  `resources_id` INT NOT NULL,
+  PRIMARY KEY (`skill_id`, `resources_id`),
+  INDEX `fk_skill_has_resources_resources1_idx` (`resources_id` ASC),
+  INDEX `fk_skill_has_resources_skill1_idx` (`skill_id` ASC),
+  CONSTRAINT `fk_skill_has_resources_skill1`
+    FOREIGN KEY (`skill_id`)
+    REFERENCES `skill` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_skill_has_resources_resources1`
+    FOREIGN KEY (`resources_id`)
+    REFERENCES `resource` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `achievement_requirement`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `achievement_requirement` ;
+
+CREATE TABLE IF NOT EXISTS `achievement_requirement` (
+  `achievement_id` INT NOT NULL,
+  `skill_requirement_id` INT NOT NULL,
+  `date_started` DATE NULL,
+  `date_completed` DATE NULL,
+  PRIMARY KEY (`achievement_id`, `skill_requirement_id`),
+  INDEX `fk_achievement_requirement_requirement_idx` (`skill_requirement_id` ASC),
+  CONSTRAINT `fk_achievement_requirement_achievement`
+    FOREIGN KEY (`achievement_id`)
+    REFERENCES `achievement` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_achievement_requirement_requirement`
+    FOREIGN KEY (`skill_requirement_id`)
+    REFERENCES `skill_requirement` (`requirement_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
 SET SQL_MODE = '';
 DROP USER IF EXISTS skilluser@localhost;
 SET SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
