2a6a4fe0345f949165070c41744dee65441e7a6d
JHowell78 <jahowell1978@gmail.com>
2019-09-16 11:03:57 -0600

    merge branden

--- Skills/src/main/java/com/skilldistillery/midterm/controllers/LogRegisterTestController.java
+++ Skills/src/main/java/com/skilldistillery/midterm/controllers/LogRegisterTestController.java
@@ -1,39 +1,23 @@
 package com.skilldistillery.midterm.controllers;
-
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
 import com.skilldistillery.midterm.data.AuthenticationDAO;
-import com.skilldistillery.midterm.entities.Skill;
-
 @Controller
 public class LogRegisterTestController {
-
-	
-	@Autowired
-	private AuthenticationDAO dao;
-	
-	@RequestMapping(path= "/")
-	public String index(Model model) {
-		List<Skill> f = dao.findAll();
-		model.addAttribute("skillset", f);
-				
-	  return "index";
-	  // return "index"; // if using a ViewResolver.
-	}
-	
-//	  @RequestMapping(path = "/")
-//	    public ModelAndView putIndexHere(Model model) {
-//	        ModelAndView mv = new ModelAndView();
-//	        mv.setViewName("index");
-//	        return mv;
-//	    }
-	
+    @Autowired
+    private AuthenticationDAO dao;
+    @RequestMapping(path = "profile.do", method = RequestMethod.POST)
+    public String index(Model model) {
+        return "skill/userProfile";
+        // return "index"; // if using a ViewResolver.
+    }
+//    @RequestMapping(path = "/")
+//      public ModelAndView putIndexHere(Model model) {
+//          ModelAndView mv = new ModelAndView();
+//          mv.setViewName("index");
+//          return mv;
+//      }
 }
-
--- Skills/src/main/webapp/WEB-INF/index.jsp
+++ Skills/src/main/webapp/WEB-INF/index.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+    pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
 <!DOCTYPE html>
@@ -9,24 +9,26 @@
 <title>Skills</title>
 </head>
 <body>
-<h1>Test</h1>
-	<h3>Login</h3>
-	<form action="login.do" method="POST">
-		<table>
-			<tr>
-				<td>UserName</td><td><input name="username" /></td>
-				<td>Password</td><td><input name="password" /></td>
-				<td><input type="submit" value="Login" /></td>
-			</tr>
-		</table>
-	</form>
-	<div>
-	
-	<c:forEach items="${skillset}" var="skill">
-
-  <h5><a href="getSkill.do?fid=${skill.id}">${skill.name} </a> (${skill.description})</h5>
-
-</c:forEach>
-</div>
+    <h1>Test</h1>
+    <h3>Login</h3>
+    <form action="profile.do" method="POST">
+        <table>
+            <tr>
+                <td>UserName</td>
+                <td><input name="username" /></td>
+                <td>Password</td>
+                <td><input name="password" /></td>
+                <td><input type="submit" value="Login" /></td>
+            </tr>
+        </table>
+    </form>
+    <div>
+        <c:forEach items="${skillset}" var="skill">
+            <h5>
+                <a href="getSkill.do?fid=${skill.id}">${skill.name} </a>
+                (${skill.description})
+            </h5>
+        </c:forEach>
+    </div>
 </body>
 </html>
