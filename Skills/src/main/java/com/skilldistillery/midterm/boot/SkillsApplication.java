6ecebd8387c361176b5e49978ed63f66eafe6bb8
Mark Ag <mk2modx@icloud.com>
2019-09-16 09:06:53 -0600

    pushing my branch with errors

--- Skills/src/main/java/com/skilldistillery/midterm/boot/SkillsApplication.java
+++ Skills/src/main/java/com/skilldistillery/midterm/boot/SkillsApplication.java
@@ -2,10 +2,21 @@ package com.skilldistillery.midterm.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class SkillsApplication {
+@ComponentScan(basePackages = "com.skilldistillery.midterm")
+@EntityScan("com.skilldistillery.midterm")
+public class SkillsApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	  return application.sources(SkillsApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SkillsApplication.class, args);
 	}
---
+++ Skills/src/main/java/com/skilldistillery/midterm/data/AuthenticationDAOImpl.java
@@ -0,0 +1,66 @@
+package com.skilldistillery.midterm.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.midterm.entities.Skill;
+import com.skilldistillery.midterm.entities.User;
+
+
+@Service
+@Transactional
+public class AuthenticationDAOImpl implements AuthenticationDAO{
+
+	@PersistenceContext
+	private EntityManager em;
+	
+	@Override
+	public Skill findSkillById(int id) {
+		
+		
+		return em.find(Skill.class, id);
+	}
+
+	@Override
+	public List<Skill> findAll() {
+		
+		
+	List<Skill> list = new ArrayList<>();
+	
+	String query = "SELECT s FROM Skill s"; //JOIN FETCH f.actors
+			
+		return list = em.createQuery(query,Skill.class).getResultList();
+	}
+	
+	
+	@Override
+	public User create(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isUserNameUnique(String username) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public User getUserByUserName(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isValidUser(User u) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
